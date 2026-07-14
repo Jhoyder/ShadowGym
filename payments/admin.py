dc4028a8d9d56d15019154a81892c89c80ea9062
Jhoyder López <hayanlopez1999@gmail.com>
2026-07-14 10:10:48 -0500

    Fix PaymentAdmin - remove duplicate register and autocomplete_fields

--- payments/admin.py
+++ payments/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from .models import Payment
 from django.utils import timezone
-# Register your models here.
+
 @admin.register(Payment)
-#no deje tocar membership_end
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ['member', 'plan', 'amount', 'membership_start', 'membership_end', 'method']
     list_filter = ['method', 'plan', 'payment_date']
     search_fields = ['member__first_name', 'member__last_name']
     readonly_fields = ['membership_end', 'payment_date']
-    autocomplete_fields = ['member', 'plan']
     
     fieldsets = (
         ('Datos del Pago', {
@@ -22,4 +20,3 @@ class PaymentAdmin(admin.ModelAdmin):
             'fields': ('notes', 'payment_date')
         }),
     )
-    admin.site.register(Payment)
