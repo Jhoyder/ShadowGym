3e9548bafb5cf90a71b76a5777a3db9aabeb4a6e
Jhoyder López <hayanlopez1999@gmail.com>
2026-07-14 10:18:03 -0500

    Update members admin - add Plan and improve Member admin

--- members/admin.py
+++ members/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-# Register your models here.
-from .models import Member
-admin.site.register(Member)
+from .models import Member, Plan
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ['name', 'duration_days', 'price']
+    list_filter = ['duration_days', 'price']
+    search_fields = ['name']
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'id_number', 'phone', 'is_active', 'membership_start', 'membership_end']
+    list_filter = ['is_active', 'membership_start', 'membership_end']
+    search_fields = ['first_name', 'last_name', 'id_number', 'phone']
+    readonly_fields = ['created_at']
+
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(Member, MemberAdmin)
