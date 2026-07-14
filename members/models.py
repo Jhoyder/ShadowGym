9f257d5fe318c80b580e6bc1314d56fef22b54dc
Jhoyder López <hayanlopez1999@gmail.com>
2026-07-14 10:16:01 -0500

    Add Plan model to members app

--- members/models.py
+++ members/models.py
@@ -1,5 +1,21 @@
 from django.db import models
-# Create your models here.
+
+class Plan(models.Model):
+    """Modelo para los planes de membresía"""
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration_days = models.IntegerField()  # Duración en días
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.duration_days} Días - ${self.price}"
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = "Plan"
+        verbose_name_plural = "Planes"
+
+
 class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -7,9 +23,15 @@ class Member(models.Model):
     phone = models.CharField(max_length=15)
     email = models.EmailField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-# Estas 3 se llenan solas cuando registras un pago
+    # Estas 3 se llenan solas cuando registras un pago
     membership_start = models.DateField(null=True, blank=True)
     membership_end = models.DateField(null=True, blank=True) 
     is_active = models.BooleanField(default=False)
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+        verbose_name = "Miembro"
+        verbose_name_plural = "Miembros"
